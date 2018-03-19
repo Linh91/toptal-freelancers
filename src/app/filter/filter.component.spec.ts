276cf99cca66f795ef6063bc4bb8168d1aa42e99
sammy.ha <sammy.ha@kingfisher.com>
2018-03-19 17:23:25 +0000

    filter component

--- src/app/filter/filter.component.spec.ts
+++ src/app/filter/filter.component.spec.ts
@@ -1,25 +1,50 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, OnInit } from '@angular/core';
+import { IFilter, ACTIONS as FilterACTIONS } from './filter-reducer';
+import { Store } from '@ngrx/store';
+import { FormGroup, FormControl } from '@angular/forms';
+import * as Rx from 'RxJS';
 
-import { FilterComponent } from './filter.component';
+@Component({
+  selector: 'app-filter',
+  template: 
+    '<form class="filter">'+
+    '<label>Name</label>'+
+    '<input type="text" [formControl]="name" name="name"/>'+
+    '<label>Email</label>'+
+    '<input type="text" [formControl]="email" name="email"/>'+
+    '<a (click)="clearFilter()" class="btn btn-default">Clear Filter</a>'+
+    '</form>',
+  styleUrls: ['./filter.component.scss'],
+})
+export class FilterComponent implements OnInit {
 
-describe('FilterComponent', () => {
-  let component: FilterComponent;
-  let fixture: ComponentFixture<FilterComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FilterComponent ]
+  public name = new FormControl();
+  public email = new FormControl();
+  constructor(private store: Store<any>) {
+    store.select('filter').subscribe((filter: IFilter) => {
+      this.name.setValue(filter.name);
+      this.email.setValue(filter.email);
     })
-    .compileComponents();
-  }));
+    Rx.Observable.merge(this.name.valueChanges, this.email.valueChanges).debounceTime(1000).subscribe(() => this.filter());
+  }
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FilterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  ngOnInit() {
+  }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  filter() {
+    this.store.dispatch({
+      type: FilterACTIONS.UPDATE_FITLER,
+      payload: {
+        name: this.name.value,
+        email: this.email.value,
+      }
+    });
+  }
+
+  clearFilter() {
+    this.store.dispatch({
+      type: FilterACTIONS.CLEAR_FITLER,
+    })
+  }
+
+}
